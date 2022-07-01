a4e369243beebe736777879608ff958b0f42ce83
Izem MOUHOUBI <81371121+izem-idem@users.noreply.github.com>
2022-07-01 12:05:17 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f1233d57.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
